a5a32fac290e18216e83b48fb2be473d0675a8b6
rosanna-smith <rosanna.smith@uq.edu.au>
2024-12-13 15:30:55 +1100

    latest reordering of layout code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { Command } from 'commander';
+import { ROCrate } from 'ro-crate';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,41 +10,6 @@ const program = new Command();
 import { fileURLToPath } from 'url';
 import { roCrateToJSON, renderTemplate } from './lib/preview.js';
 
-
-program
-  .name("load_ro_crate")
-  .description("Load an RO-Crate from a specified directory")
-  .argument("<path_to_crate_directory>", "Path to the crate directory")
-  .option(
-    "-l, --layout <layoutPath>",
-    "Path to the layout file",
-    path.join(__dirname, "lib", "default_layout.json")
-  )
-
-  .action(async (cratePath, options) => {
-    if (!fs.existsSync(cratePath) || !fs.lstatSync(cratePath).isDirectory()) {
-      console.error(`Error: ${cratePath} is not a valid directory`);
-      return;
-    }
-    const metadataFile = path.join(cratePath, "ro-crate-metadata.json");
-  if (!fs.existsSync(metadataFile)) {
-    console.error(`Error: Metadata file not found in ${cratePath}`);
-    return;
-  }
-
-  const metadata = JSON.parse(fs.readFileSync(metadataFile, "utf8"));
-  const templateFile = path.join(__dirname, "template.html");
-  const template = fs.readFileSync(templateFile, "utf8");
-  const layoutPath = options.layout;
-  const layout = JSON.parse(fs.readFileSync(layoutPath, "utf8"));
-  const crateLite = await roCrateToJSON(metadata, layout);
-  const html = renderTemplate(crateLite, template, layout)
-    fs.writeFileSync(
-      path.join(cratePath, "ro-crate-preview.html"),
-      html,
-      "utf-8"
-    );
-  
 // Fetch the mapping JSON between conformsTo and mode
 async function fetchMapping() {
   const mappingLocation = 'https://raw.githubusercontent.com/Language-Research-Technology/ro-crate-modes/refs/heads/main/conformsToMapping.json';
@@ -68,7 +35,7 @@ async function fetchJsonFromMode(mode) {
   return await response.json();
 }
 
-async function processData() {
+async function findLayout(crate, TODO) {
   try {
     // Fetch the mapping
     const conformsToMapping = await fetchMapping();
@@ -78,32 +45,68 @@ async function processData() {
     }
 
     // Get the conformsTo dynamically from the input JSON (metadata needs to be defined)
-    const conformsToLookup = metadata["@graph"][1].conformsTo["@id"];
+    const conformsToLookup = crate.rootDataset.conformsTo?.[0]?.['@id'];
     console.log(`conformsTo lookup: ${conformsToLookup}`);
 
     // Check if the conformsTo exists in the mapping
-    if (conformsToMapping.hasOwnProperty(conformsToLookup)) {
+    if (conformsToLookup && conformsToMapping.hasOwnProperty(conformsToLookup)) {
       const mode = conformsToMapping[conformsToLookup];
       
       // Fetch the inputGroups from the mode
       const jsonData = await fetchJsonFromMode(mode);
       
       if (jsonData.inputGroups) {
-        const layoutFile = jsonData.inputGroups;
+        const layout = jsonData.inputGroups;
         // console.log("layoutFile:", layoutFile);
         console.log("layoutFile found.");
+        return layout
       } else {
         console.log("No 'inputGroups' key found in the JSON file.");
-      }
-    } else {
+      }}
       console.log(`conformsTo ${conformsToLookup} not found in the key-value list. Using default layout.`);
-      const layoutFile = path.join(__dirname, 'lib/default_layout.json'); //TODO
-      console.log("Using default layout:", layoutFile);
-    }
+      const layoutFile = path.join(__dirname, 'lib/default_layout.json'); //TODO pass this in instead of path
+      const layout = JSON.parse(fs.readFileSync(layoutFile, "utf8"));
+      return layout
   } catch (error) {
     console.error("Error processing data:", error);
   }
 }
-processData()
+
+program
+  .name("load_ro_crate")
+  .description("Load an RO-Crate from a specified directory")
+  .argument("<path_to_crate_directory>", "Path to the crate directory")
+  .option(
+    "-l, --layout <layoutPath>",
+    "Path to the layout file",
+    path.join(__dirname, "lib", "default_layout.json")
+  )
+
+  .action(async (cratePath, options) => {
+    if (!fs.existsSync(cratePath) || !fs.lstatSync(cratePath).isDirectory()) {
+      console.error(`Error: ${cratePath} is not a valid directory`);
+      return;
+    }
+    const metadataFile = path.join(cratePath, "ro-crate-metadata.json");
+  if (!fs.existsSync(metadataFile)) {
+    console.error(`Error: Metadata file not found in ${cratePath}`);
+    return;
+  }
+
+  const metadata = JSON.parse(fs.readFileSync(metadataFile, "utf8"));
+  const templateFile = path.join(__dirname, "template.html");
+  const template = fs.readFileSync(templateFile, "utf8");
+  const crate = new ROCrate(metadata, { array: true, link: true });
+  await crate.resolveContext();
+  const crateLite = roCrateToJSON(metadata);
+  const layout = await findLayout(crate, options.layout);
+  const html = renderTemplate(crateLite, template, layout)
+    fs.writeFileSync(
+      path.join(cratePath, "ro-crate-preview.html"),
+      html,
+      "utf-8"
+    );
+  
+
   });
 program.parse(process.argv);
